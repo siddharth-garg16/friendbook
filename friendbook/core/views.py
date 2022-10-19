9b5dbd0f583957f9152f7adbc07bdc17e74e5713
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-10-20 01:04:48 +0530

    signup function

--- friendbook/core/views.py
+++ friendbook/core/views.py
@@ -1,9 +1,31 @@
 import re
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
 
 def index(request):
     return render(request, "index.html")
 
 def signup(request):
-    return render(request, "signup.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password==password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "This email is already taken.")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "This username is already taken.")
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+        else:
+            messages.info(request, "Passwords didn't match.")
+            return redirect('signup')
+    else:
+        return render(request, "signup.html")
