8edacb75be9186501c00e714d28984985b5429b4
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-10-20 14:57:48 +0530

    signin and logout functions

--- friendbook/core/views.py
+++ friendbook/core/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
-
+@login_required(login_url='signin')
 def index(request):
     return render(request, "index.html")
 
@@ -24,8 +26,35 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+                #creating profile object for the new user
+                user_model = User.objects.get(username)
+                new_profile = User.objects.create_user(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, "Passwords didn't match.")
             return redirect('signup')
     else:
         return render(request, "signup.html")
+
+def signin(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, "Invalid credentials")
+            return render(request, 'signin.html')
+    else:
+        return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+def setting(request):
+    pass
